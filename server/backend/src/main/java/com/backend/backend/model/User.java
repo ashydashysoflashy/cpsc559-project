ad3671625200e854d45c2996449da9fc8d33e33f
bmsmcgee <98934884+bmsmcgee@users.noreply.github.com>
2025-03-02 20:13:26 -0700

    created User model and began filling in

---
+++ server/backend/src/main/java/com/backend/backend/model/User.java
@@ -0,0 +1,32 @@
+package com.backend.backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "users", uniqueConstraints = {
+    @UniqueConstraint(columnNames = {
+        "username"
+    }),
+    @UniqueConstraint(columnNames = {
+        "email"
+    })
+})
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setID(Long id) {
+        this.id = id;
+    }
+    
+}
