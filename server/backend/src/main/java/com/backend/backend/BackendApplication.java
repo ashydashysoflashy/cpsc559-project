ba63c2785b56044911ece61206ff78948bc28f64
bmsmcgee <98934884+bmsmcgee@users.noreply.github.com>
2025-03-02 19:46:00 -0700

    Configured spring boot to use Java 8 date/time converters and UTC timezone

--- server/backend/src/main/java/com/backend/backend/BackendApplication.java
+++ server/backend/src/main/java/com/backend/backend/BackendApplication.java
@@ -1,10 +1,25 @@
 package com.backend.backend;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
+@EntityScan(basePackageClasses = {
+	BackendApplication.class,
+	Jsr310JpaConverters.class
+})
 public class BackendApplication {
+
+	@PostConstruct
+	void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
